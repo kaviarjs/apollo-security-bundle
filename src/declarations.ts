a2ee363cdf1e160b438a7ec3cc8d9adb6f015376
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-07-20 18:03:34 +0300

    (fix) UserId

--- src/declarations.ts
+++ src/declarations.ts
@@ -1,11 +1,12 @@
 import "@kaviar/graphql-bundle";
+import { UserId } from "@kaviar/security-bundle";
 
 declare module "@kaviar/graphql-bundle" {
   export interface IGraphQLContext {
     /**
      * The userId retrieved from the request. Also verified if the session is valid.
      */
-    userId: any;
+    userId: UserId;
     /**
      * The token retrieved from either HTTP Cookies, HTTP Headers, or WebSocket parameter
      */
